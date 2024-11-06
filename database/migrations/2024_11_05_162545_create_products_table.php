a26bb3a3a6feacf563665768a01bead589be17d9
Abhishekdeveloper1 <abhicodergenx@gmail.com>
2024-11-06 14:28:44 +0530

    little change

--- database/migrations/2024_11_05_162545_create_products_table.php
+++ database/migrations/2024_11_05_162545_create_products_table.php
@@ -13,25 +13,6 @@ return new class extends Migration
     {
         Schema::disableForeignKeyConstraints();
         Schema::create('products', function (Blueprint $table) {
-            // $table->id();
-            // $table->string('name');
-            // $table->text('description');
-            // $table->string('sku');
-            // $table->integer('category_id')->nullable();
-            // $table->integer('subcategory_id')->nullable();
-            // $table->integer('inventory_id')->nullable();
-            // $table->integer('discount_id')->nullable();  
-            // $table->bigInteger('price');  
-            // $table->string('image1')->nullable();
-            // $table->string('image2')->nullable();
-            // $table->string('image3')->nullable();
-            // $table->string('image4')->nullable();
-            // $table->string('image5')->nullable();
-            // $table->boolean('status')->default(0);
-            // $table->timestamps();
-            // $table->foreign('category_id')->references('id')->on('product_categories')->onDelete('cascade');
-            // $table->foreign('subcategory_id')->references('id')->on('productsub_categories')->onDelete('cascade');
-            // $table->foreign('inventory_id')->references('id')->on('product_inventories')->onDelete('set null');
             $table->id();
             $table->string('name');
             $table->text('description');
